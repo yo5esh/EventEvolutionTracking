b2b8088805b81faae67a114c3ef0685cd279de43
Rishi <rishikaley@gmail.com>
2019-10-16 14:23:22 +0530

    Updated preprocessing

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # DM-Project-10
+
+* For pos tagging : run this cmnd from ./Datasets/stanford-postaer-2018-10-16/ folder
+    java -mx300m -classpath stanford-postagger.jar edu.stanford.nlp.tagger.maxent.MaxentTagger -model models/gate-EN-twitter-fast.model -textFile ../../untagged_tweets.txt > ../../tagged_tweets2.txt
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,40 +1,56 @@
 import pandas as pd
+from subprocess import check_output
+import numpy as np
 
-# df = pd.read_csv('../Datasets/CrisisNLP_labeled_data_crowdflower/2013_Pakistan_eq/2013_Pakistan_eq_CF_labeled_data.tsv', error_bad_lines=False, sep='\t')
-# #print(df.head())
+noun_tags = ['NN', 'NNS', 'NNP']
+DATASET_NAME = './Datasets/CrisisNLP_labeled_data_crowdflower/2013_Pakistan_eq/2013_Pakistan_eq_CF_labeled_data.tsv'
+FINAL_NAME = DATASET_NAME.split('/')[3]
 
-# df.drop(['label'],axis=1,inplace=True)
-#print('Changed labels\n',df.head())
+def snowflake2utcms(sf):
+    #print(sf," - ",sf[1:-1])
+    sf = int(sf[1:-1])
+    return ((sf >> 22) + 1288834974657)
 
-##################### storing in .txt for sending for tagging pos
-# f = open("../untagged_tweets.txt", "w")
-# tweets = df['tweet_text']
-# for tweet in tweets:
-#     f.write(tweet)
-#     f.write('.\n')
-# f.close()
-
-'''
-For pos tagging : run this cmnd from ./Datasets/stanford-postaer-2018-10-16/ folder
-    java -mx300m -classpath stanford-postagger.jar edu.stanford.nlp.tagger.maxent.MaxentTagger -model models/gate-EN-twitter-fast.model -textFile ../../untagged_tweets.txt > ../../tagged_tweets2.txt
-'''
-
-
-##################### filtering for nouns
-noun_tags = ['NN']
-f = open("../tagged_tweets.txt", "r")
-all_tweets = f.read().split('\n')
-f.close()
-f = open("../filtered_tweets.txt", "w")
-for tweet in all_tweets:
+def getNouns(tweet):
+    proctweet = ''
     words = tweet.split(' ')
-    for word in words:
-        word = word.split('_')
+    for word1 in words:
+        word = word1.split('_')
         if len(word) < 2:
-            print(tweet,'\n')
-        elif word[1] in noun_tags:
-            f.write(word[0])
-            f.write(' ')
-            
-    f.write('\n')
-f.close()
+            #print(tweet,'\n')
+            continue
+        elif word[0][0:3] == 'http':
+            continue
+        elif word[1] in noun_tags or word[0][0]=='#':
+            proctweet += (word[0])
+            proctweet += ' '
+    return proctweet
+
+df = pd.read_csv(DATASET_NAME, error_bad_lines=False, sep='\t')
+df.drop(['label'],axis=1,inplace=True)
+print('Removed labels')
+df['tweet_timeStamp'] = df['tweet_id'].map(snowflake2utcms)
+print('Added timestamps column')
+print(df.head())
+
+# storing in .txt for sending for tagging pos
+np.savetxt("../untagged_tweets.txt", df.values, fmt='%s')
+
+print('Started tagging all tweets')
+taggerProcess = check_output(["java", "-mx300m", "-classpath", "Taggers/stanford-postagger-2018-10-16/stanford-postagger.jar", "edu.stanford.nlp.tagger.maxent.MaxentTagger", "-model", "Taggers/stanford-postagger-2018-10-16/models/gate-EN-twitter-fast.model", "-textFile", "./untagged_tweets.txt", "-l"])
+all_tweets = (taggerProcess.decode('utf-8')).split('\n')
+print('Tagged all tweets')
+
+# # filtering for nouns
+filtered_tweets = []
+for tweet in all_tweets:
+    if tweet != '':
+        filtered_tweets.append(getNouns(tweet))
+
+print(len(filtered_tweets))
+print(df.tail(1))
+df['filt_tweet_text'] = filtered_tweets
+print('Added filtered tweets column')
+#print(df.head())
+df.to_csv(f'./Datasets/PreprocessedData/{FINAL_NAME}.csv', sep='\t', index=False)
+print('Saved to csv file')
